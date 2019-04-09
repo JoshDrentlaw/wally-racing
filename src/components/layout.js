3659ffa9d27aab189eb1c527b5a3f34d8a6b3908
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-08 17:53:38 -0700

    Added tailwind and styled components. Starting on grid layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,21 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import styled from "styled-components"
+import tw from "tailwind.macro"
+
 import Header from "./header"
-import "./layout.css"
+import "../global.css"
+
+const Body = styled.div`
+  display: grid;
+  template-grid-areas: 
+    "header header header"
+    ". content ."
+    ". content ."
+    "footer footer footer"
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,20 +29,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer></footer>
-        </div>
-      </>
+      <Body>
+        <Header siteTitle={data.site.siteMetadata.title} style={{ gridArea: `header` }} />
+        <main style={{ gridArea: `content` }}>{children}</main>
+        <footer style={{ gridArea: `footer` }}></footer>
+      </Body>
     )}
   />
 )
