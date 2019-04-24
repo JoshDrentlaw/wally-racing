3f160c8d78e9290da42d7bdad1113df2f4420cbe
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-23 22:21:24 -0700

    Styled nav for mobile and desktop. Working on hamburger button.

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ import styled from "styled-components"
 import tw from "tailwind.macro"
 
 const Nav = styled.nav`
-  ${tw`sticky bg-transparent z-50`}
+  ${tw`sticky flex items-center justify-between flex-wrap bg-transparent z-50`}
   top: 0;
   height: 50px;
   grid-area: header;
 `
 
 const LinkContainer = styled.div`
-  ${tw`relative inline-flex flex-col flex-grows items-center float-right lg:w-1/5 lg:flex-row`}
+  ${tw`w-full block flex-col flex-grow lg:flex lg:items-center lg:justify-end lg:flex-row lg:w-auto`}
 
   .active {
     ${tw`font-bold`}
@@ -31,16 +31,21 @@ const LinkContainer = styled.div`
   }
 `
 
-
-
 const Header = () => (
   <Nav>
-    <img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/c_scale,co_rgb:000000,e_colorize:100,h_50/v1555171073/Wally%20Pankratz%20Racing%20School/WallyPankratz.webp" alt="Wally Pankratz signature" />
+    <img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/c_scale,h_50,e_outline:1/v1555171073/Wally%20Pankratz%20Racing%20School/WallyPankratz.webp" alt="Wally Pankratz signature" className="mr-auto" />
+    <div className="block mr-4 lg:hidden">
+      <button className="flex items-center px-3 py-2 border rounded border-white hover:text-white hover:border-black">
+        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+      </button>
+    </div>
     <LinkContainer>
-      <Link to="/" activeClassName="active">Home</Link>
-      <Link to="/about/" activeClassName="active">About</Link>
-      <Link to="/stats/" activeClassName="active">Stats</Link>
-      <Link to="/pricing/" activeClassName="active">Pricing</Link>
+      <div className="flex flex-col items-end mr-2 lg:w-1/4 lg:flex-row lg:justify-around">
+        <Link to="/" activeClassName="active">Home</Link>
+        <Link to="/about/" activeClassName="active">About</Link>
+        <Link to="/stats/" activeClassName="active">Stats</Link>
+        <Link to="/pricing/" activeClassName="active">Pricing</Link>
+      </div>
     </LinkContainer>
   </Nav>
 )
