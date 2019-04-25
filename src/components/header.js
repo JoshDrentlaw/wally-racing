e5a62ed8965091e1bd145eab216337b06acdcd6c
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-25 08:28:00 -0700

    Working on hamburger button.

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import styled from "styled-components"
@@ -19,7 +19,7 @@ const LinkContainer = styled.div`
   }
 
   & a {
-    ${tw`text-black text-center no-underline p-2 lg:sw-1/4`}
+    ${tw`text-white lg:text-black text-center no-underline p-2 lg:sw-1/4`}
 
     &:focus {
       ${tw`font-bold`}
@@ -31,23 +31,34 @@ const LinkContainer = styled.div`
   }
 `
 
-const Header = () => (
-  <Nav>
-    <img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/c_scale,h_50,e_outline:1/v1555171073/Wally%20Pankratz%20Racing%20School/WallyPankratz.webp" alt="Wally Pankratz signature" className="mr-auto" />
-    <div className="block mr-4 lg:hidden">
-      <button className="flex items-center px-3 py-2 border rounded border-white hover:text-white hover:border-black">
-        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-      </button>
-    </div>
-    <LinkContainer>
-      <div className="flex flex-col items-end mr-2 lg:w-1/4 lg:flex-row lg:justify-around">
-        <Link to="/" activeClassName="active">Home</Link>
-        <Link to="/about/" activeClassName="active">About</Link>
-        <Link to="/stats/" activeClassName="active">Stats</Link>
-        <Link to="/pricing/" activeClassName="active">Pricing</Link>
+const Header = () => {
+  useEffect(() => {
+    const button = document.getElementById('hamburger');
+    toggle(button);
+  })
+
+  function toggle(el) {
+    console.log(el);
+  }
+
+  return (
+    <Nav>
+      <img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/c_scale,h_50,e_outline:1/v1555171073/Wally%20Pankratz%20Racing%20School/WallyPankratz.webp" alt="Wally Pankratz signature" className="mr-auto" />
+      <div className="block mr-4 lg:hidden">
+        <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-black text-black">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+        </button>
       </div>
-    </LinkContainer>
-  </Nav>
-)
+      <LinkContainer>
+        <div className="flex flex-col items-end pr-2 bg-black lg:bg-transparent lg:w-1/4 lg:flex-row lg:justify-around">
+          <Link to="/" activeClassName="active">Home</Link>
+          <Link to="/about/" activeClassName="active">About</Link>
+          <Link to="/stats/" activeClassName="active">Stats</Link>
+          <Link to="/pricing/" activeClassName="active">Pricing</Link>
+        </div>
+      </LinkContainer>
+    </Nav>
+  )
+}
 
 export default Header
