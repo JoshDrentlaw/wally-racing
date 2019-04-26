42f0490470548f2c2719e5e78b3ac84b10ed5fcd
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-04-26 12:43:02 -0700

    Fixed drop down menu state. Adjusted nav styles.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import tw from "tailwind.macro"
 
 const Nav = styled.nav`
-  ${tw`sticky flex items-center justify-between flex-wrap bg-transparent z-50`}
+  ${tw`sticky flex items-center justify-between flex-wrap bg-black z-100`}
   top: 0;
   height: 50px;
   grid-area: header;
@@ -19,7 +19,7 @@ const LinkContainer = styled.div`
   }
 
   & a {
-    ${tw`text-white lg:text-black text-center no-underline p-2 lg:sw-1/4`}
+    ${tw`text-white text-center no-underline p-2 lg:sw-1/4`}
 
     &:focus {
       ${tw`font-bold`}
@@ -32,7 +32,7 @@ const LinkContainer = styled.div`
 `
 
 const Menu = styled.div`
-  ${tw`flex flex-col items-end pr-2 bg-black lg:bg-transparent lg:w-1/4 lg:flex-row lg:justify-around`}
+  ${tw`flex flex-col items-end pr-2 bg-black lg:w-1/4 lg:flex-row lg:justify-around z-10`}
 
   visibility: ${props => props.visible ? 'visible' : 'hidden'};
   transform: ${props => props.visible ? `translateY(0%)` : `translateY(-100%)` };
@@ -40,13 +40,13 @@ const Menu = styled.div`
 `
 
 const Header = () => {
-  const [visible, setVisible] = useState('false');
+  const [visible, setVisible] = useState(false);
 
   return (
     <Nav>
       <img src="https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/c_scale,h_50,e_outline:1/v1555171073/Wally%20Pankratz%20Racing%20School/WallyPankratz.webp" alt="Wally Pankratz signature" className="mr-auto" />
       <div className="block mr-4 lg:hidden">
-        <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-black text-black" onClick={() => {setVisible(!visible)}}>
+        <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-white text-white" onClick={() => {setVisible(!visible)}}>
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
         </button>
       </div>
