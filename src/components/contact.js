fc023221093fa4c43591664bb0de981537a78f51
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-05-03 08:30:34 -0700

    Adding label transitions to contact form.

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,24 +4,31 @@ import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
 const Form = styled.form`
-    ${tw`w-1/3 mx-auto p-4 flex flex-col items-center`}
+    ${tw`w-1/3 mx-auto p-4 flex flex-col items-center border border-black rounded`}
 `
 
 const FormGroup = styled.div`
-    ${tw``}
-    input~label {
-        color: red;
-    }
+    ${tw`mt-4 -mb-2`}
 `
 
 const Label = styled.label`
     ${tw`block focus:font-bold`}
 
-    /* transform: translateY(20px); */
+    transform: translateY(-20px);
+    transition: all 0.2s ease;
 `
 
 const Input = styled.input`
     ${tw`block border-b border-black`}
+    
+    &::placeholder {
+        ${tw`text-transparent`}
+    }
+
+    &:focus + label {
+        transform: translate(200px, 220px);
+        transform: scale(0.8);
+    }
 `
 
 const Contact = (props) => {
@@ -29,8 +36,8 @@ const Contact = (props) => {
     return (
         <Form>
             <FormGroup>
-                <Label for="email">Email</Label>
                 <Input id="email" type="email" placeholder="Email" />
+                <Label for="email">Email</Label>
             </FormGroup>
         </Form>
     )
