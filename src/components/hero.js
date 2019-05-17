e4c3ad8a19c7e64a933edecdb2d267a11fbf03c7
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-05-16 18:03:13 -0700

    Changed .png's to .jpeg's. Hoping this will fix Safari.

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,7 +8,7 @@ const Img = styled.img`
     ${tw`md:h-screen w-full`}
 `
 
-const url = "https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/v1555381845/Wally%20Pankratz%20Racing%20School/shapeimage_1" + (is.safari() ? '.png' : '.webp');
+const url = "https://res.cloudinary.com/josh-drentlaw-web-development/image/upload/v1555381845/Wally%20Pankratz%20Racing%20School/shapeimage_1" + (is.safari() ? '.jpeg' : '.webp');
 
 const Hero = () => (
     <Img src={url} alt="Ford Focus Midget Car" />
