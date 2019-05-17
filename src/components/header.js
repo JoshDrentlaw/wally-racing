5a8f3eaabbabb024193b235e36070ae641cc3d96
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-05-16 22:21:06 -0700

    Menu toggle just decided to start working...

--- src/components/header.js
+++ src/components/header.js
@@ -55,7 +55,7 @@ function Header() {
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
         </button>
       </div>
-      <Menu className="menu" visible mobile>
+      <Menu visible={visible} mobile={mobile}>
         <A to="/" activeClassName="active">Home</A>
         <A to="/about/" activeClassName="active">About</A>
         <A to="/stats/" activeClassName="active">Stats</A>
