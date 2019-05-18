92a7965f0b7780742a434d7eb6491e056037ffc7
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-05-18 00:27:11 -0700

    Nav menu slides in and out in mobile properly

--- src/components/header.js
+++ src/components/header.js
@@ -6,21 +6,23 @@ import styled from "styled-components"
 import tw from "tailwind.macro"
 
 const Nav = styled.nav`
-  ${tw`sticky flex items-center justify-between flex-wrap bg-black`}
+  ${tw`sticky`}
   top: 0;
   height: 60px;
 `
 
-const Menu = styled.div`
-  ${props => console.log(`mobile: ${props.mobile} visible: ${props.visible}`)};
+const Wrapper = styled.div`
+  ${tw`w-full flex items-center justify-between flex-wrap bg-black z-0`}
+  height: 60px;
+`
 
-  ${tw`w-full bg-black flex flex-col items-end lg:flex lg:items-center lg:justify-around lg:flex-row lg:w-1/5`};
+const Menu = styled.div`
+  ${tw`w-full block bg-black flex flex-col items-end lg:flex lg:items-center lg:justify-around lg:flex-row lg:w-1/5`};
 
   visibility: ${props => props.mobile ? `${props.visible ? 'visible' : 'hidden'}` : 'visible' };
-
   transform: ${props => props.mobile ? `${props.visible ? `translateY(0%)` : `translateY(-100%)`}` : `translateY(0%)`};
-
   transition: all 200ms ease-out;
+  z-index: ${props => props.mobile ? '-10' : '10'};
 
   .active {
     ${tw`font-bold`}
@@ -49,18 +51,21 @@ function Header() {
 
   return (
     <Nav>
-      <img src={url} alt="Wally Pankratz signature" className="h-full ml-3" />
-      <div className="block mr-3 md:hidden">
-        <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-white text-white" onClick={() => setVisible(!visible)}>
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-        </button>
-      </div>
-      <Menu visible={visible} mobile={mobile}>
-        <A to="/" activeClassName="active">Home</A>
-        <A to="/about/" activeClassName="active">About</A>
-        <A to="/stats/" activeClassName="active">Stats</A>
-        <A to="/pricing/" activeClassName="active">Pricing</A>
-      </Menu>
+      <Wrapper>
+        <img src={url} alt="Wally Pankratz signature" className="h-full ml-3" />
+        <div className="block mr-3 md:hidden">
+          <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-white text-white" onClick={() => setVisible(!visible)}>
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        <Menu visible={visible} mobile={mobile}>
+          <A to="/" activeClassName="active">Home</A>
+          <A to="/about/" activeClassName="active">About</A>
+          <A to="/stats/" activeClassName="active">Stats</A>
+          <A to="/pricing/" activeClassName="active">Pricing</A>
+        </Menu>
+      </Wrapper>
+      
     </Nav>
   )
 }
