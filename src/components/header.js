edf014ee9e3c5a08aa88d218ea59d8c6a9a8b71c
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-07-15 00:02:51 -0700

    Removed hamburger menu.

--- src/components/header.js
+++ src/components/header.js
@@ -16,19 +16,8 @@ const Wrapper = styled.div`
   height: 60px;
 `
 
-const Menu = styled.div.attrs(props => ({
-  visibility: (props.visible ? 'visible' : 'hidden'),
-  transform: (props.visible ? `translateY(0%)` : `translateY(-100%)`),
-}))`
-  ${tw`w-full block bg-black flex flex-col items-end lg:flex lg:items-center lg:justify-around lg:flex-row lg:w-1/5`};
-  visibility: visible;
-
-  @media(max-width: 1024px) {
-    visibility: ${props => props.visibility };
-    transform: ${props => props.transform };
-    transition: all 200ms ease-out;
-  }
-  
+const Menu = styled.div`
+  ${tw`bg-black inline-flex flex-col items-end lg:flex lg:items-center lg:justify-around lg:flex-row lg:w-1/5`};
 
   .active {
     ${tw`font-bold`}
@@ -48,7 +37,6 @@ const A = styled(Link)`
 `
 
 function Header({ data }) {
-  const [visible, setVisible] = useState();
   
   return (
     <StaticQuery
@@ -57,12 +45,7 @@ function Header({ data }) {
         <Nav>
           <Wrapper>
             <Img className="h-full ml-3" fixed={data.file.childImageSharp.fixed} />
-            <div className="block mr-3 lg:hidden">
-              <button id="hamburger" className="flex items-center px-3 py-2 border rounded border-white text-white" onClick={() => setVisible(!visible)}>
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-              </button>
-            </div>
-            <Menu visible={visible}>
+            <Menu>
               <A to="/" activeClassName="active">Home</A>
             </Menu>
           </Wrapper>
