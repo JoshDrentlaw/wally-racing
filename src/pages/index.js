1c3a882652b0105e7d07f700efb4e8c9372ee86b
Josh Drentlaw <jdrentlaw@gmail.com>
2020-02-12 20:00:57 -0800

    Updated packages. Removed some content and updated book link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Hero from '../components/hero'
-import Card from '../components/card'
+// import Hero from '../components/hero'
+// import Card from '../components/card'
 import Contact from '../components/contact'
 
 import styled from "styled-components"
@@ -30,9 +30,9 @@ const P = styled.p`
   text-align: ${props => props.align || "center"};
 `
 
-const Cards = styled.section`
+/* const Cards = styled.section`
   ${tw`w-full flex md:flex-row flex-col justify-between`}
-`
+` */
 
 const VideoContainer = styled.div`
     width: calc(100% - 1rem);
@@ -48,7 +48,7 @@ const VideoContainer = styled.div`
     }
 `
 
-const bookLink = "https://www.amazon.com/Quarter-Midget-Chassis-Technology-Steve/dp/0936834544/ref=sr_1_2?crid=3SLEPN8T9S53B&keywords=quarter+midget+chassis+technology&qid=1563158936&s=gateway&sprefix=midget+chassi%2Caps%2C194&sr=8-2";
+const bookLink = "https://www.amazon.com/dp/B00LFSE5RA/ref=cm_sw_em_r_mt_dp_U_WJmrEbN2WZHWR";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -71,14 +71,14 @@ const IndexPage = ({ data }) => (
       "midget racecar school",
       "midget racecar school socal",
     ]} />
-    <Hero data={data.muddy.childImageSharp.fluid} />
+    {/* <Hero data={data.muddy.childImageSharp.fluid} /> */}
     <div className="lg:w-4/5 mx-auto p-4">
       <Section>
         <H1>Welcome to Wally Pankratz Racing School.</H1>
         <P>Who is Wally Pankratz?</P>
         <Img fixed={data.profile.childImageSharp.fixed} alt="Wally Pankratz at Ventura Raceway." />
         <P>Wally Pankratz started racing in 1970. Over his long career he won four Championships with 114 Main Event wins and many awards including induction into the Belleville National Midget Hall of Fame and the Legends of Ascot.  In addition, Wally collaborated with Steve Smith Autosports to develop the book <a href={bookLink} target="_blank" rel="noopener noreferrer">"Midget Chassis Technology"</a> for Midget racers.  This Chassis set up book is dedicated to introducing the racer to Midget racing while teaching the finer points of the set-up and racing in this class.</P>
-        <P>The Ford Focus Midget Course is designed for drivers who want to learn the skills essential for Short Track Oval racing. Students will work "One on One" with Wally to understand and master corner entrance, car rotation, throttle pick up points, unwinding of the car, smooth inputs of throttle, brakes and steering. Thirteen on track sessions that make up 200 laps on a 1/4 mile pavement track.</P>
+        <P>The Ford Focus Midget Course is designed for drivers who want to learn the skills essential for Short Track Oval racing. Students will work "One on One" with Wally to understand and master corner entrance, car rotation, throttle pick up points, unwinding of the car, smooth inputs of throttle, brakes and steering.</P>
         <P align="left">
           Also available:
           <ul>
@@ -91,7 +91,7 @@ const IndexPage = ({ data }) => (
 
       <Section style={{maxWidth: "640px", margin: "0 auto"}}>
         <VideoContainer>
-          <iframe src="https://player.vimeo.com/video/353074662" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/353074662" frameborder="0" allow="autoplay; fullscreen" title="Wally Pankratz Racing School" allowfullscreen></iframe>
           <p><a href="https://vimeo.com/353074662">Wally Pankratz Racing School</a> from <a href="https://vimeo.com/kingmediaco">King Media Co</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
         </VideoContainer>
       </Section>
